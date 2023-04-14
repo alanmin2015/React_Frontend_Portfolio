530dd07456b5ba1f45e6252cd5d012e1b0bb6d0f
alanmin2015 <alanmin2015@gmail.com>
2023-04-14 17:31:48 -0400

    projects commit

--- my-portfolio-app/src/components/Project/Project.js
+++ my-portfolio-app/src/components/Project/Project.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import './Project.css';
 
 function Project() {
   const [projects, setProjects] = useState([]);
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
     fetch('https://admin.wenbomin.ca/api/projects')
       .then(response => response.json())
-      .then(data => setProjects(data))
+      .then(data => setProjects(data));
+      
+    fetch('https://admin.wenbomin.ca/api/types')
+      .then(response => response.json())
+      .then(data => setTypes(data));
   }, []);
 
+  console.log(types);
+
+ 
   return (
-    <div className="App">
+    <div className="AppProjects">
       <h1>My Projects</h1>
       <div className="projects">
         {projects.map(project => (
           <div className="project" key={project.id}>
-            <img src="https://wenbomin.ca/image/1.jpg" alt={project.name} />
-            <h2>{project.name}</h2>
+             {console.log(types.find(type => type.id === project.type_id)?.title)}
+            <img src={project.image}  alt={project.title} />
+            <h2>{project.title}- {types.find(type => type.id === project.type_id)?.title}
+</h2>
+<p>{project.type.title}</p>
             <p>{project.content}</p>
-            <p>{project.image} </p>
+            <a href={project.url}>Github</a>             
           </div>
         ))}
       </div>
